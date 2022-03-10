a6109a0d183b11631c946959c10fe3ff72513a31
venous666 <99412095+venous666@users.noreply.github.com>
2022-03-10 12:28:07 -0300

    Add files via upload

---
+++ comida.js
@@ -0,0 +1,25 @@
+inteiro = 10
+real = 10.45
+cancelou = true 
+comida1= "milho"
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+comida2= "pipoca"
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+comida3= " abacate"
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+comida4= "palmito"
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+comida5= "maracuja"
+console.log(inteiro, real, cancelou, comida1, comida2, comida3, comida4, comida5)
