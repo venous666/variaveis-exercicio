f3c4c5e491910281fd6f7b905f521b412208787b
venous666 <99412095+venous666@users.noreply.github.com>
2022-03-10 12:52:27 -0300

    Add files via upload

---
+++ nomesobrenome.js
@@ -0,0 +1,27 @@
+nome1= "Maria"
+nome2= "Caire"
+sobrenome1 = "Venus"
+sobrenome2=" Maia"
+console.log(nome1, sobrenome1, nome2, sobrenome2)
+
+//4
+nome3= "caire"
+nome4= "kylo"
+nome5= "leia"
+nome6= "gerard"
+nome7= "jaiza"
+nome8= "lucas"
+
+militou = true
+lacrou= true
+morreu = true
+tankou= false
+redipillou= false
+crinjou= true
+
+console.log(nome3, militou)
+console.log(nome4, lacrou)
+console.log(nome5,morreu)
+console.log(nome6, tankou)
+console.log(nome7, redipillou)
+console.log(nome8, crinjou)
