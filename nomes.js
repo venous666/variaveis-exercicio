eb4968930f9de643afdd66e47fdc9b616c5542eb
venous666 <99412095+venous666@users.noreply.github.com>
2022-03-10 12:28:23 -0300

    Add files via upload

---
+++ nomes.js
@@ -0,0 +1,17 @@
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome1 = "Marx"
+
+
+inteiro = 10
+real = 10.45
+cancelou = true
+nome2= "Engels"
+
+inteiro = 10
+real = 10.45
+cancelou = true 
+nome3= "Dorian Gray"
+
+console.log(inteiro, real, cancelou, nome1, nome2, nome3)
