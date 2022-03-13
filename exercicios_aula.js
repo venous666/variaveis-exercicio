4fe1b01a3e2dfa3f34a4b3e3c89494baaa1844f7
venous666 <99412095+venous666@users.noreply.github.com>
2022-03-13 18:59:48 -0300

    Add files via upload

---
+++ exercicios_aula.js
@@ -0,0 +1,69 @@
+5//
+numero1= "1"
+numero2= "2"
+numero3="3"
+numero4="4"
+numero5= "5"
+numero6="6"
+numero7="7"
+numero8="8"
+console.log(numero1,numero2,numero3,numero4,numero5,numero6,numero7,numero8)
+//7
+const subtrair= function( x, y){
+let resultado = x - y 
+return "subtração: (" + x + ") - (" + y + ") = " + resultado
+}
+
+
+console.log(subtrair(40, 20))
+console.log(subtrair(50, 20))
+
+//8
+const divisão = function( x, y){
+    let resultado = x / y 
+    return "divisão: (" + x + ") / (" + y + ") = " + resultado
+    }
+    
+    
+    console.log(dividir(40, 2))
+    console.log(dividir(50, 2))
+    
+    //9
+    const multiplicar = function( x, y){
+        let resultado = x * y 
+        return "multplicação: (" + x + ") * (" + y + ") = " + resultado
+        }
+        
+        
+        console.log(multiplicar(40, 2))
+        console.log(multiplicar(50, 2))
+        
+        //10
+        const resto = function ( x, y){
+        let resultado = x % y
+        return "resto: (" + x +") % (" + y +") = " + resultado
+        }
+        console.log(resto(100, 10))
+        console.log(resto(108, 16))
+
+        //11
+        let num =  100;
+        const valor = num * 5/100;
+        console.log(valor)
+        //12
+        const numero_de_produtos = 20;
+        const valor_dos_produtos = 15;
+        const total = numero_de_produtos * valor_dos_produtos
+        console.log( total)
+        //13
+        variaveis= "teste, do, senai, trabalho";
+        resultado = variaveis.length;
+        console.log = (resultado)
+
+        //14
+        const x = 3
+        const y = 3
+        valor = 3**3
+        console.log (valor)
+        //15
+        console.log( math.sqrt(49));
